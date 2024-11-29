64b4b3cd1c3ec8ec887a5c132b8dde593b21bfe0
Kugelblitz25 <vighneshnayak203@gmail.com>
2024-11-29 20:14:55 +0530

    camera

---
+++ cctv/main.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap = cv2.VideoCapture("rtsp://ncair:ncair@10.185.153.173/cam/realmonitor?channel=2&subtype=1")
+
+while(True):
+    ret, frame = cap.read()
+    if ret != 0:
+        cv2.imshow('frame',frame)           
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
